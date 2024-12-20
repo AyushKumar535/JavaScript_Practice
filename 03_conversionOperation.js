c12986e96fb0e19cbf649932626e98471d828fa0
AyushKumar535 <ayushrana03dec2003@gmail.com>
2024-12-20 19:58:54 +0530

    03_conversion_datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+let score1 = null
+let score2 = undefined
+
+console.log (typeof score)      //string
+console.log (typeof(score))     //string
+
+
+let valueInNumber = Number(score2)
+console.log(typeof valueInNumber)       //number
+
+console.log(valueInNumber)
+
+
+
+//Conversion :-
+// "33" -> 33
+// "33abc" -> NaN
+//  1 -> true ; 0 -> false
+//  "" -> false
+// "Ayush" -> true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber) 
