eaa44813dbeb85d5cfe75e481ee0d4b8ff867562
AyushKumar535 <ayushrana03dec2003@gmail.com>
2024-12-21 22:07:51 +0530

    array1.js

---
+++ 11_array1.js
@@ -0,0 +1,29 @@
+const marvel_heroes = ["Thor","Ironman","Spiderman"]
+const dc_heroes = ["superman","flash","batman"]
+
+//Conatenate
+const all_Heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_Heroes) 
+
+
+//Spread
+const all_new_heroes = [...marvel_heroes,...dc_heroes]
+console.log(all_new_heroes)
+
+
+const another_array = [1,2,3,[4,5,6],7,8,9,[6,7,[4,3]]]
+const another_array_2 = another_array.flat(2)
+console.log(another_array_2)
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log("A ->", score1,score2,score3)
+console.log("B ->",Array.of(score1,score2,score3))
