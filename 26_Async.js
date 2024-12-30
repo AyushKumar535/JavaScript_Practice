830ea8558f1f028c10fb8f26c1cf796e7d415aa9
AyushKumar535 <ayushrana03dec2003@gmail.com>
2024-12-30 06:44:55 +0530

    Async.js

---
+++ 26_Async.js
@@ -0,0 +1,17 @@
+// JavaScript is Synchronous :- one after one there will be execution
+                        //      It's a single threaded (runtime environment)
+
+
+// Execution Control :-
+        // execute one line of code at a time
+            // -> console log 1                 (each operation execute after completing the first execution)
+            // -> console log 2
+
+
+//    Blocking Code            vs          Non Blocking Code 
+//1. Block the flow of                   Does not block execution
+//      Program
+//2. Read file Sync                      Read file Async
+
+
+// Js Engine , Web API , Fetch API
